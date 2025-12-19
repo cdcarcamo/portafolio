4769bb9878c54696d6c1c0ba4bd705c766f9c774
cdcarcamo <carlosdcarcamos@gmail.com>
2025-12-19 01:13:02 -0500

    Se realizamn cambio con los importes de main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from app.routers.views import router as views_router
+from pathlib import Path
 
 app = FastAPI(title="Cárcamo System Company")
+
+BASE_DIR = Path(__file__).resolve().parent
 
 # Servir carpeta static
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
