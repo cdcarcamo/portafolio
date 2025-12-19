0f41343f0228b815d8edbbd73a1d8cde156b49a5
cdcarcamo <carlosdcarcamos@gmail.com>
2025-12-18 23:32:58 -0500

    Se elimina importe de libreria jinja en main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 # Proyecto de portafolio personal
 
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from app.routers.views import router as views_router
 
