813a2a1489e917d157ec620436b8b289d7a11a22
cdcarcamo <carlosdcarcamos@gmail.com>
2025-12-19 23:30:48 -0500

    Se modifica acceso de la app en el main, se modifican etiquetas en base

--- app/main.py
+++ app/main.py
@@ -5,12 +5,16 @@ from fastapi.staticfiles import StaticFiles
 from app.routers.views import router as views_router
 from pathlib import Path
 
+
+
+
 app = FastAPI(title="Cárcamo System Company")
 
-BASE_DIR = Path(__file__).resolve().parent
-
-# Servir carpeta static
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
 
 app.include_router(views_router)
 
